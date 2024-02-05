94760403fdc07c729f0e3ae5458e32e0c260b836
Alex Ivy-Townley <alextownleymakes@gmail.com>
2024-02-05 09:17:56 -0600

    some style fixes

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -21,18 +21,18 @@ const Todo = ({ todo, todoFunctions }) => {
 
     return (
         <div className="todo-item">
+            <div className="timestamp">{formatDate(updateDate)}</div>
+            <textarea
+                value={editedTitle}
+                onChange={(e) => setEditedTitle(e.target.value)}
+                disabled={!editMode}
+                className={`todo-content ${editMode ? 'tc-em' : ''}`}
+            />
             <input
                 type="checkbox"
                 checked={completed}
                 onChange={handleCheckboxChange}
             />
-            <input
-                type="text"
-                value={editedTitle}
-                onChange={(e) => setEditedTitle(e.target.value)}
-                disabled={!editMode}
-            />
-            <span className="timestamp">{formatDate(updateDate)}</span>
             {editMode ?
             <>
             <button onClick={handleSaveClick}>Save</button>
